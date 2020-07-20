fdc968fa23a7bafc163755ab6469d2a5b2821bac
Developer <developer@Mac-de-Developer.local>
2020-07-20 09:14:24 -0700

    installing the necessary dependencies for navigation with React navigation 5

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import {Provider as PaperProvider, Avatar, Button} from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
 
 function App() {
   return (
     <PaperProvider>
-      <SafeAreaView style={styles.container}>
-        <Text>Findit sales!</Text>
-        <Avatar.Text size={24} label="XD" />
-        <Button
-          icon="camera"
-          mode="contained"
-          onPress={() => console.log('Pressed')}>
-          Press me
-        </Button>
-      </SafeAreaView>
+      <NavigationContainer>
+        <SafeAreaView style={styles.container}>
+          <Text>Findit sales!</Text>
+          <Avatar.Text size={24} label="XD" />
+          <Button
+            icon="camera"
+            mode="contained"
+            onPress={() => console.log('Pressed')}>
+            Press me
+          </Button>
+        </SafeAreaView>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
